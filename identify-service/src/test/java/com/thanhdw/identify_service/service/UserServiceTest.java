acf162c28e2a65eb34df0276929a62a357465d1f
thanhdw <dunt@goodgame.vn>
2025-04-26 17:13:25 +0700

    test jacoco

--- identify-service/src/test/java/com/thanhdw/identify_service/service/UserServiceTest.java
+++ identify-service/src/test/java/com/thanhdw/identify_service/service/UserServiceTest.java
@@ -10,21 +10,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 @TestPropertySource("/test.properties")
 public class UserServiceTest {
     
     @Autowired
     private UserService userService;
+    
     @MockitoBean
     private UserRepository userRepository;
     
@@ -54,7 +58,8 @@ public class UserServiceTest {
     void createUser_validRequest_success() {
         //xay dung test
         // GIVEN - tao du lieu
-        when(userRepository.existsByUsername(anyString())).thenReturn(false);//false thi thuc hien cac buoc kiem tra tiep theo
+        when(userRepository.existsByUsername(anyString())).thenReturn(
+                false);//false thi thuc hien cac buoc kiem tra tiep theo
         when(userRepository.save(any())).thenReturn(user);
         // WHEN - thuc hien
         userService.createUser(userCreationRequest);
@@ -65,16 +70,44 @@ public class UserServiceTest {
         Assertions.assertThat(userResponse.getLastName()).isEqualTo("Duong");
         
     }
+    
     @Test
     void createUser_userExisted_fail() {
         //xay dung test
         // GIVEN - tao du lieu
-        when(userRepository.existsByUsername(anyString())).thenReturn(true);// true bat ngay vi tri nay nen khong can thuc hien cac buoc kiem tra tiep theo
+        when(userRepository.existsByUsername(anyString())).thenReturn(
+                true);// true bat ngay vi tri nay nen khong can thuc hien cac buoc kiem tra tiep theo
         // WHEN - thuc hien
         
-        var exception = assertThrows(AppException.class, ()->
-                userService.createUser(userCreationRequest));
+        var exception = assertThrows(AppException.class, () -> userService.createUser(userCreationRequest));
         //THEN - kiem tra
         Assertions.assertThat(exception.getErrorCode().getCode()).isEqualTo(1001);
+    }
+    
+    @Test
+    //Anotation @WithMockUser la cua spring security de tao ra mot user da dang nhap
+    @WithMockUser(
+            username = "thanhdwf"
+    )
+    void getMyInfo_valid_success() {
+        //Phuong thuc se gap loi do getMyInfo() lay token tu SecurityContextHolder nen khong can phai set token
+        //can them dependency cho phuong thuc spring security test
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.of(user));
+        var response = userService.getMyInfo();
+        Assertions.assertThat(response.getUsername()).isEqualTo("thanhdwf");
+        Assertions.assertThat(response.getId()).isEqualTo("ccccc");
+    }
+    @Test
+    //Anotation @WithMockUser la cua spring security de tao ra mot user da dang nhap
+    @WithMockUser(
+            username = "thanhdwf"
+    )
+    void getMyInfo_userNotFound_error() {
+        //Phuong thuc se gap loi do getMyInfo() lay token tu SecurityContextHolder nen khong can phai set token
+        //can them dependency cho phuong thuc spring security test
+        when(userRepository.findByUsername(anyString())).thenReturn(Optional.ofNullable(null));
+        var exception = assertThrows(AppException.class, () -> userService.getMyInfo());
+        
+        Assertions.assertThat(exception.getErrorCode().getCode()).isEqualTo(1006);
     }
 }
